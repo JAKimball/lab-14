955d54e911f0660549182804f09f99b30d120f1d
Jonathan A. Kimball <jonathan.kimball64@gmail.com>
2019-09-26 17:57:54 -0700

    migration sql complete

---
+++ data/migrations/1569540672268-normalize-bookshelf.sql
@@ -0,0 +1,26 @@
+-- Normalize Bookshelves
+
+-- Create the bookshelves table
+CREATE TABLE bookshelves (id SERIAL PRIMARY KEY,name VARCHAR(255));
+
+-- Populate the bookshelves table with the distinct bookshelf values from 
+-- the books table.
+INSERT INTO bookshelves(name) SELECT DISTINCT bookshelf FROM books;
+-- Confirm success
+SELECT count(*) FROM bookshelves;
+
+-- Add a bookshelf_id column to the books table to serve as the foreign key to
+-- the bookshelf table.
+ALTER TABLE books ADD COLUMN bookshelf_id INT;
+
+-- Based on the bookshelf feild, populate the bookshelf_id column of books
+-- with the primary key of the corresponding bookshelf record. 
+UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf WHERE books.bookshelf = shelf.name;
+-- Confirm success
+SELECT bookshelf_id FROM books;
+
+-- Remove the newly redundant column
+ALTER TABLE books DROP COLUMN bookshelf;
+
+-- Add the foreign key constraint
+ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
